b250e28036c8556bd0c7d70658837d6558b0681b
EC2 Default User <ec2-user@ip-172-31-42-87.ap-northeast-3.compute.internal>
2024-06-17 18:32:10 +0000

    検索機能復活

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,20 +4,28 @@ class SearchesController < ApplicationController
     @search_type = params[:search_type]
     @search_category = params[:search_category]
 
+    Rails.logger.debug "Search Query: #{@query}"
+    Rails.logger.debug "Search Type: #{@search_type}"
+    Rails.logger.debug "Search Category: #{@search_category}"
+
     if @query.present?
       case @search_category
-      when 'user'
+      when 'ユーザー'
         @users = User.looks(@search_type, @query)
+        Rails.logger.debug "User search results: #{@users.to_a}"
         @posts = Post.none
         @tags = Tag.none
-      when 'post'
+      when 'レビュー'
         @users = User.none
         @posts = Post.looks(@search_type, @query)
+        Rails.logger.debug "Post search results: #{@posts.to_a}"
         @tags = Tag.none
-      when 'tag'
+      when 'タグ'
         @users = User.none
         @tags = Tag.where("name LIKE ?", "%#{@query}%")
+        Rails.logger.debug "Tag search results: #{@tags.to_a}"
         @posts = Post.joins(:tags).where(tags: { id: @tags.pluck(:id) })
+        Rails.logger.debug "Posts by tag search results: #{@posts.to_a}"
       else
         @users = User.none
         @posts = Post.none
